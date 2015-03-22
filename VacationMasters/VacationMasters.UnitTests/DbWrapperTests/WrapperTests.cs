b7da607aa7079d2dfc575d9e32d9785d862bff6f
Emanuel Scirlet <emanuel.scirlet@outlook.com>
2015-03-22 16:42:00 +0100

    added wrappers

---
+++ VacationMasters/VacationMasters.UnitTests/DbWrapperTests/WrapperTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MySql.Data.MySqlClient;
+using NUnit.Framework;
+using VacationMasters.Wrappers;
+
+namespace VacationMasters.UnitTests.DbWrapperTests
+{
+    [TestFixture]
+    public class WrapperTests
+    {
+        private IDbWrapper _dbWrapper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dbWrapper = new DbWrapper();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void QueryWrapperTest()
+        {
+            var sql = "Select ID from teste";
+            var x = _dbWrapper.QueryValue<string>(sql);
+            x.Should().Be("100");
+        }
+
+        [Test]
+        public void GivenNormalConditionsConnectionToMySqlServerShouldNotFail()
+        {
+            var connection = _dbWrapper.GetConnection();
+            var connected = connection.State == ConnectionState.Open;
+            Assert.IsTrue(connected);
+        }
+    }
+}
---
+++ VacationMasters/VacationMasters/Wrappers/DbWrapper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace VacationMasters.Wrappers
+{
+    public class DbWrapper:IDbWrapper
+    {
+        private MySqlConnection CreateConnection()
+        {
+            var connection = new MySqlConnection("server=galactica.emanuelscirlet.com;database=vacationmasters;uid=sa;password=predator12;");
+            return connection;
+        }
+
+        private MySqlCommand CreateCommand(MySqlConnection connection)
+        {
+            var command = new MySqlCommand {Connection = connection};
+            return command;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            var connection = CreateConnection();
+            connection.Open();
+            return connection;
+        }
+
+        public MySqlCommand GetCommand(MySqlConnection connection)
+        {
+            var command = CreateCommand(connection);
+            command.Parameters.Clear();
+            command.CommandType = CommandType.Text;
+            return command;
+        }
+
+        private async void MessageBox(string msg)
+        {
+            var msgDlg = new Windows.UI.Popups.MessageDialog(msg) { DefaultCommandIndex = 1 };
+            await msgDlg.ShowAsync();
+        }
+
+        private void RunCommand(Action<MySqlCommand> func)
+        {
+            RunCommand(c =>
+            {
+                func(c);
+                return true;
+            });
+        }
+
+        private T RunCommand<T>(Func<MySqlCommand, T> func)
+        {
+            try
+            {
+                using (var connection = GetConnection())
+                {
+                    using (var command = GetCommand(connection))
+                    {
+                        return func(command);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox("An error occured! Please check your connection!");
+                return default(T);
+            }
+        }
+      
+        public T QueryValue<T>(string sqlQuery)
+        {
+            return RunCommand(command =>
+            {
+                command.CommandText = sqlQuery;
+                var result = command.ExecuteScalar();
+                if (result == null || result == DBNull.Value)
+                {
+                    return default(T);
+                }
+                Type t = Nullable.GetUnderlyingType(typeof (T)) ?? typeof (T);
+                return (T) Convert.ChangeType(result, t);
+            });
+        }
+    }
+}
