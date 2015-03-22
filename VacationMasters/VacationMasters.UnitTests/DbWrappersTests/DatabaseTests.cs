cb03ba157f96a19e3d7859ed275aa22efef5f10a
Emanuel Scirlet <emanuel.scirlet@outlook.com>
2015-03-22 01:54:34 +0100

    Testing infrastructure
    Added unit test infrastructure

---
+++ VacationMasters/VacationMasters.UnitTests/DbWrappersTests/DatabaseTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace VacationMasters.UnitTests.DbWrappersTests
+{
+    [TestFixture]
+    class DatabaseTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+
+        [Test]
+        public void DummyTest()
+        {
+            int x = 0;
+        }
+    }
+}
