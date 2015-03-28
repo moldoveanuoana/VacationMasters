02ace60cdfe1dc4dae92fd3c849c56f01f2199e3
Emanuel Scirlet <emanuel.scirlet@outlook.com>
2015-03-29 00:13:17 +0100

    Added User class

---
+++ VacationMasters/VacationMasters/Essentials/User.cs
@@ -0,0 +1,28 @@
+﻿namespace VacationMasters.Essentials
+{
+    public class User
+    {
+        public string PhoneNumber { get; set; }
+        public string KeyWordSearches { get; set; }
+        public string UserName { get; private set; }
+        public string Type { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Banned { get; set; }
+        public string Email { get; set; }
+
+        public User(string userName, string firstName, string lastName, string email, string phoneNumber,
+            bool banned, string type, string keyWordSearches)
+        {
+            UserName = userName;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Banned = banned;
+            Type = type;
+            KeyWordSearches = keyWordSearches;
+        }
+
+    }
+}
