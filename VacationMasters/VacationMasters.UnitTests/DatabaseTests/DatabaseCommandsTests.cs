03a19b50dff659324c42ace7927c4fe32ad399ac
Emanuel Scirlet <emanuel.scirlet@outlook.com>
2015-03-29 01:28:32 +0100

    UserManager updates

--- VacationMasters/VacationMasters.UnitTests/DatabaseTests/DatabaseCommandsTests.cs
+++ VacationMasters/VacationMasters.UnitTests/DatabaseTests/DatabaseCommandsTests.cs
@@ -1,4 +1,7 @@
 ﻿using NUnit.Framework;
+using VacationMasters.Essentials;
+using VacationMasters.UnitTests.Infrastructure;
+using VacationMasters.UserManagement;
 using VacationMasters.Wrappers;
 
 namespace VacationMasters.UnitTests.DatabaseTests
@@ -6,17 +9,49 @@ namespace VacationMasters.UnitTests.DatabaseTests
     class DatabaseCommandsTests
     {
         private IDbWrapper _dbWrapper;
+        private IUserManager _userManagement;
 
         [SetUp]
         public void SetUp()
         {
             _dbWrapper = new DbWrapper();
+            _userManagement = new UserManagement.UserManager(_dbWrapper);
         }
 
         [Test]
-        public void InsertNewUserShouldNotThrow()
+        public void LMDShouldNotThrow()
         {
-           //I need classes that match DB schema
+            var password = CreateRandom.String();
+            var user = new User(CreateRandom.String(), CreateRandom.String(), CreateRandom.String(),
+                CreateRandom.String(), CreateRandom.String(), false, CreateRandom.String(), CreateRandom.String());
+
+            _userManagement.AddUser(user, password);
+            _userManagement.RemoveUser(user.UserName);
+        }
+
+        public void MultipleUsersCanLoginAtTheSameTime()
+        {
+            //TODO:this
+        }
+
+        public void ValidUserWithPasswordPassCredentialChecking()
+        {
+            //TODO:this
+        }
+
+        public void ValidUserWithWrongPasswordFailsCredentialChecking()
+        {
+            //TODO:this
+        }
+
+        public void NonExistingUserTriesToLogin()
+        {
+            //TODO:this
+        }
+
+        public void UserCanChangeHisPassword()
+        {
+            //TODO:this
         }
     }
 }
