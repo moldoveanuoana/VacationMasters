29553c532fd5b77fbd7a21f8baff661764816dc6
Emanuel Scirlet <emanuel.scirlet@outlook.com>
2015-04-03 21:29:08 +0200

    various fixes

--- VacationMasters/VacationMasters/Essentials/User.cs
+++ VacationMasters/VacationMasters/Essentials/User.cs
@@ -1,7 +1,15 @@
-﻿namespace VacationMasters.Essentials
+﻿using System;
+
+namespace VacationMasters.Essentials
 {
     public class User
     {
+        public enum AuthTypes
+        {
+            User,
+            Administrator
+        };
+
         public string PhoneNumber { get; set; }
         public string KeyWordSearches { get; set; }
         public string UserName { get; private set; }
@@ -10,6 +18,7 @@
         public string LastName { get; set; }
         public bool Banned { get; set; }
         public string Email { get; set; }
+        public AuthTypes AuthType { get; set; }
 
         public User(string userName, string firstName, string lastName, string email, string phoneNumber,
             bool banned, string type, string keyWordSearches)
