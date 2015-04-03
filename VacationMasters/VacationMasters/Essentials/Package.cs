fa0ebd9fab7eda2b9e35d338e2a1c6ce96dfbe7c
IrinaButaru <irina.butaru@yahoo.com>
2015-04-03 10:26:10 -0700

    Var2(Inceput pachete)

---
+++ VacationMasters/VacationMasters/Essentials/Package.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationMasters.Essentials
+{
+    class Package
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Included { get; set; }
+        public string Transport { get; set; }
+        public double Price { get; set; }
+        public double SearchIndexRate { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public byte[] Picture { get; set; }
+        
+
+        public Package(string name,string type,string included,string transport,double price,double searchIndexRate,
+                               DateTime beginDate,DateTime endDate,byte[] picture)
+        {
+            Name = name;
+            Type = type;
+            Included = included;
+            Transport = transport;
+            Price = price;
+            BeginDate = beginDate;
+            EndDate = endDate;
+            Picture = picture;
+
+        }
+
+        public Package()
+        {
+            
+        }
+    }
+}
