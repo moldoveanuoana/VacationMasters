0a2a169e5bf99a7133fedde60cdb304ce4184609
mihaisa20 <mihai.salavastru@my.fmi.unibuc.ro>
2015-05-16 16:52:26 +0300

    Added Groups to Register

---
+++ VacationMasters/VacationMasters/Essentials/GroupManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationMasters.Wrappers;
+
+namespace VacationMasters.Essentials
+{
+    public class GroupManager
+    {
+        private readonly IDbWrapper _dbWrapper;
+        public GroupManager(IDbWrapper dbWrapper)
+        {
+            _dbWrapper = dbWrapper;
+        }
+
+        public List<string> GetAllGroups()
+        {
+            return _dbWrapper.RunCommand(command =>
+            {
+                command.CommandText = "Select Name from Groups";
+                var reader = command.ExecuteReader();
+                var list = new List<string>();
+                while (reader.Read())
+                {
+                    var name = reader.GetString(0);
+                    list.Add(name);
+                }
+                return list;
+            });
+        }
+    }
+}
